d5236a5c8a04df536a0d31c66de86b5d5089f23c
Robert Kirkman <misternumberone@live.com>
2023-10-09 18:11:24 -0500

    implement replays

--- src/main.c
+++ src/main.c
@@ -2,104 +2,93 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include <time.h>
 #include <MiniFB.h>
 
 // Define constants for the framebuffer size and colors
+
+// 1111 1111 0000 0000 0000 0000 0000 0000
+// 0xFF 11111111 255
+//
 #define WIDTH 800
 #define HEIGHT 600
-#define WHITE MFB_ARGB(0xFF, 0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF)
-#define BLACK MFB_ARGB(0xFF, 0x0, 0x0, 0x0)
+#define WHITE MFB_ARGB(0xFF, 0xFF, 0xFF, 0xFF)
+#define BLACK MFB_ARGB(0xFF, 0x00, 0x00, 0x00)
 
 // Structure to store Cartesian point data
-typedef struct _Point
+typedef struct Dot
 {
-    size_t x, y;
-} Point;
+    int x, y, radius;
+} Dot;
 
-// Convert polar coordinates to Cartesian coordinates within the bounds of our framebuffer
-Point polarToCartesian(double rho, double phi)
+typedef struct DotList DotList;
+struct DotList
 {
-    Point p;
-    double x = rho * cos(phi) + WIDTH / 2;
-    double y = rho * sin(phi) + HEIGHT / 2;
+    DotList *next;
+    Dot p;
+    clock_t timestamp;
+};
 
-    // Ensure the resulting point is within the framebuffer bounds
-    p.x = (x >= 0 && x < WIDTH) ? (size_t)x : 0;
-    p.y = (y >= 0 && y < HEIGHT) ? (size_t)y : 0;
-
-    return p;
-}
-
-// Draw a gradient square dot into the given framebuffer
-void drawDot(uint32_t *framebuffer, Point p, unsigned radius)
+// Draw a circular dot into the given framebuffer
+void drawDot(uint32_t *fb, Dot p)
 {
-    if (p.x < radius || p.y < radius || p.x >= WIDTH || p.y >= HEIGHT)
-        return;
-
-    for (size_t i = p.x - radius; i < p.x + radius && i < WIDTH; i++)
+    for (int i = p.x - p.radius; i < p.x + p.radius && i < WIDTH; i++)
     {
-        for (size_t j = p.y - radius; j < p.y + radius && j < HEIGHT; j++)
+        for (int j = p.y - p.radius; j < p.y + p.radius && j < HEIGHT; j++)
         {
-            uint32_t color = (abs((int)i - (int)p.x) + abs((int)j - (int)p.y)) * 20;
-            framebuffer[i + WIDTH * j] = MFB_ARGB(0xFF, color, color, color);
+            double dist = sqrt(pow((double)i - (double)p.x, 2) + pow((double)j - (double)p.y, 2));
+            if (dist <= p.radius && i >= 0 && j >= 0)
+            {
+                uint32_t color = (uint32_t)dist;
+                fb[i + WIDTH * j] = MFB_ARGB(0xFF, 0x00, color, 0x00);
+            }
         }
     }
 }
 
-int main(void)
+void addDot(struct mfb_window *window, uint32_t *fb, DotList **pl, int radius, clock_t timestamp)
 {
-    struct mfb_window *window = mfb_open_ex("minifbproject", WIDTH, HEIGHT, WF_RESIZABLE);
-
-    if (!window)
-        return 1;
-
-    int state, count = 0, decreasing = 0, size = 5, delay = 5;
-    double rho = 100, phi = 0;
-
-    uint32_t *framebuffer = malloc(WIDTH * HEIGHT * sizeof(uint32_t));
-
-    // Initialize the framebuffer with white
-    for (size_t i = 0; i < WIDTH * HEIGHT; i++)
+    DotList *last = NULL;
+    while (*pl != NULL)
     {
-        framebuffer[i] = WHITE;
+        last = *pl;
+        *pl = (*pl)->next;
     }
 
+    *pl = malloc(sizeof(struct DotList));
+    (*pl)->p.x = mfb_get_mouse_x(window);
+    (*pl)->p.y = mfb_get_mouse_y(window);
+    (*pl)->p.radius = radius;
+    (*pl)->timestamp = timestamp;
+    (*pl)->next = NULL;
+    if (last != NULL)
+        last->next = *pl;
+    drawDot(fb, (*pl)->p);
+}
+
+DotList *captureDots(uint32_t *fb)
+{
+    int state, size = 5;
+    DotList *head = NULL, *pl = NULL;
+    clock_t start = clock();
+    struct mfb_window *window = mfb_open_ex("recording dots...", WIDTH, HEIGHT, WF_RESIZABLE);
     do
     {
-        // Update polar coordinates based on mouse position and button clicks
-        Point cartesianFromPolar = polarToCartesian(rho, phi);
-        Point mouse;
-        mouse.x = (size_t)mfb_get_mouse_x(window);
-        mouse.y = (size_t)mfb_get_mouse_y(window);
-        int leftMouseButton = mfb_get_mouse_button_buffer(window)[1];
-        int rightMouseButton = mfb_get_mouse_button_buffer(window)[2];
+        int leftMouseButton = mfb_get_mouse_button_buffer(window)[MOUSE_BTN_1];
+        int rightMouseButton = mfb_get_mouse_button_buffer(window)[MOUSE_BTN_2];
+        int spaceBar = mfb_get_key_buffer(window)[KB_KEY_SPACE];
+
         size += leftMouseButton - rightMouseButton;
 
         if (size < 1)
             size = 1;
 
-        // Draw dots at the polar and mouse positions
-        drawDot(framebuffer, cartesianFromPolar, 5);
-        drawDot(framebuffer, mouse, size);
+        if (spaceBar)
+            addDot(window, fb, &pl, size, clock() - start);
+        if (head == NULL)
+            head = pl;
 
-        // Update the frame count, increasing it and decreasing it between 0 and delay (5)
-        // this means 60 / (5 + 5) times per second, or 6 times per second
-        if (decreasing)
-            count--;
-        else
-            count++;
-
-        // Increase the angle of the dot for plotting a circle
-        // at intervals relative to the framerate
-        if (count == delay)
-        {
-            phi += 0.1;
-            decreasing = 1;
-        }
-        if (count == 0)
-            decreasing = 0;
-
-        state = mfb_update_ex(window, framebuffer, WIDTH, HEIGHT);
+        state = mfb_update_ex(window, fb, WIDTH, HEIGHT);
 
         if (state < 0)
         {
@@ -107,7 +96,64 @@ int main(void)
             break;
         }
     } while (mfb_wait_sync(window));
+    return head;
+}
 
+void playbackDots(uint32_t *fb, DotList *pl)
+{
+    int state, size;
+    clock_t start = clock();
+    struct mfb_window *window = mfb_open_ex("displaying recorded dots...", WIDTH, HEIGHT, WF_RESIZABLE);
+    do
+    {
+        if (pl != NULL && clock() >= start + pl->timestamp)
+        {
+            drawDot(fb, pl->p);
+            pl = pl->next;
+        }
+
+        state = mfb_update_ex(window, fb, WIDTH, HEIGHT);
+
+        if (state < 0)
+        {
+            window = NULL;
+            break;
+        }
+    } while (mfb_wait_sync(window));
+}
+
+void freeDots(DotList *pl)
+{
+    DotList *tmp;
+    while (pl != NULL)
+    {
+        tmp = pl->next;
+        free(pl);
+        pl = tmp;
+    }
+}
+
+void clearFramebuffer(uint32_t *fb)
+{
+    // Initialize the framebuffer with white
+    for (size_t i = 0; i < WIDTH * HEIGHT; i++)
+    {
+        fb[i] = WHITE;
+    }
+}
+
+int main(void)
+{
+    uint32_t *framebuffer = malloc(WIDTH * HEIGHT * sizeof(uint32_t));
+    if (framebuffer == NULL)
+        return 2;
+
+    clearFramebuffer(framebuffer);
+    DotList *dotlist = captureDots(framebuffer);
+    clearFramebuffer(framebuffer);
+    playbackDots(framebuffer, dotlist);
+
+    freeDots(dotlist);
     free(framebuffer);
 
     return 0;
